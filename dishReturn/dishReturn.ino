f6ab6ea39ab4b0d68dfaa988593e406f8ac65858
s-vangala <svangala@vt.edu>
2024-11-20 17:40:53 -0500

    Update dishReturn.ino

--- dishReturn/dishReturn.ino
+++ dishReturn/dishReturn.ino
@@ -19,29 +19,33 @@ char keys[rows][colm] =
 byte rowPins[rows] = {7, 6, 5, 4}; 
 byte colPins[colm] = {3, 2, 1, 0}; 
 
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, rows, colm);
+Keypad hokieKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, rows, colm);
 
 char inputList[100];
 int inputIndex = 0;
 
+int hokieID = -1;
+char dishType = ' ';
+int numDishes = 0;
 
 void setup() 
 {
+  // put your setup code here, to run once:
   Serial.begin(9600); //this is the so that when the program starts, 
                       //it will initialize the serial communication at a baud rate of 9600
                       //the baud rate is is the speed at which the data is transmitter over the serial communication
                       //the serial communication is a method of sending and recieving data one bit at a 
                       //time over a single wire or channel
-  // put your setup code here, to run once:
- for (int i = 0; i < rows; i++)
- {
-  pinMode(rowsPins[i], OUTPUT); // Set the key pin as input with pull-up resistor [8, 10]
- }
+  Serial.println("Enter your hokieID (0-9): ");
+//  for (int i = 0; i < rows; i++)
+//  {
+//   pinMode(rowsPins[i], OUTPUT); // Set the key pin as input with pull-up resistor [8, 10]
+//  }
 
- for (int i = 0; i < colm; i++)
- {
-  pinMode(colPins[i], INPUT_PULLUP); // Set the key pin as input with pull-up resistor [8, 10]
- }
+//  for (int i = 0; i < colm; i++)
+//  {
+//   pinMode(colPins[i], INPUT_PULLUP); // Set the key pin as input with pull-up resistor [8, 10]
+//  }
 
 }
 //test
@@ -49,47 +53,65 @@ void setup()
 void loop() 
 {
   // put your main code here, to run repeatedly:
-  char customKey = customKeypad.getKey();
+  char key = keypad.getKey();
 
-  if (customKey)
+  if (key)
   {
-    inputList[inputIndex] = customKey;
-    inputIndex++;
-    Serial.print(customKey); //prints out which key is pressed
+    if (hokieID == -1)
+    {
+      if (key >= '0' && key <= '9')
+      {
+        hokieID = key - '0';
+        Serial.print("hokieID: ");
+        Serial.println(hokieID);
+        Serial.println("Enter dish type (A-D): ")
+      }
+    }
+    else if (dishType == ' ')
+    {
+      if (key >= 'A' && key <= 'D')
+      {
+        dishType = key;
+        Serial.print("dishType: ");
+        Serial.println(dishType);
+        Serial.println("Enter number of dishes (1-3): ")
+      }
+    }
+    else if (numDishes == 0)
+    {
+      if (key >= '1' && key <= '3')
+      {
+        numDishes = key - '0';
+        Serial.print("number of dishes: ");
+        Serial.println(numDishes);
+      }
+    }
+    // inputList[inputIndex] = key;
+    // inputIndex++;
+    // Serial.print(key); //prints out which key is pressed
   }
-
   // int keyState = digitalRead(keyPin); // Read the state of the key pin [8, 10]
 
   // if (keyState == LOW) 
   // { // Check if key is pressed (LOW means pressed with pull-up resistor) [8, 10]
-
   //     // Determine which key is pressed based on your keyboard connection
-
   //     char keyChar = 'A'; // Replace with the correct character for the pressed key
-
   //     Serial.print(keyChar); // Print the key character to the serial monitor [2, 3, 8]
-
   // }
 
-  for (int row = 0; row < ROWS; row++) 
-  {
-
-    digitalWrite(rowPins[row], LOW);  
-
-    for (int col = 0; col < colm; colm++) {
-
-      if (digitalRead(colPins[colm]) == LOW) {
-
-        Serial.print("Key Pressed: "); 
-
-        Serial.println(keys[row][colm]); 
-
-      }
-    }
-
-    digitalWrite(rowPins[row], HIGH); 
-
-  }
-
-  delay(10); // Debounce delay
+  // for (int row = 0; row < ROWS; row++) 
+  // {
+  //   digitalWrite(rowPins[row], LOW);  
+  //   for (int col = 0; col < colm; colm++) 
+  //   {
+  //     if (digitalRead(colPins[colm]) == LOW) 
+  //     {
+  //       Serial.print("Key Pressed: ");
+  //       Serial.println(key);
+  //       Serial.println(keys[row][colm]);
+  //     }
+  //   }
+  //   digitalWrite(rowPins[row], HIGH);
+  // }
+  // delay(10); // Debounce delay
 }
